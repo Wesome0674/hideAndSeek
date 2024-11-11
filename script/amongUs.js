912bf308586990a7c682dfcbd4056c2d28725111
Killian <killian.boularand@outlook.fr>
2024-11-11 16:56:11 +0100

    fdgdg

--- script/amongUs.js
+++ script/amongUs.js
@@ -1,5 +1,24 @@
+const spots = [
+  {
+    img: "assets/img/png/red-among.png",
+    posX: "185px",
+    posY: 0,
+  },
+  {
+    img: "assets/img/png/red-among.png",
+    posX: 0,
+    posY: 0,
+  },
+  {
+    img: "assets/img/png/red-among.png",
+    posX: "185px",
+    posY: "200px",
+  },
+];
+
 const user = document.getElementById("user");
 const layer = document.getElementById("layer");
+const map = document.querySelector(".hiding-spots");
 
 var emergency = new Audio("/assets/sounds/emergencySound.mp3");
 
@@ -14,3 +33,13 @@ emergencyButton.addEventListener("click", () => {
     layer.style.display = "none";
   }, 1000);
 });
+
+map.innerHTML = spots
+  .map((spot) => {
+    return `<div id="spot" style="position: absolute; left:${spot.posX}; top:${spot.posY};">
+              <img src="${spot.img}" />
+            </div>`;
+  })
+  .join("");
+
+const spot = document.getElementById("spot");

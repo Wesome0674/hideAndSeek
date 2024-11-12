bbaabb0370422f619870f54440eae389c056a487
Killian.bld <105119247+Wesome0674@users.noreply.github.com>
2024-11-12 12:57:08 +0100

    logic beggin

--- script/amongUs.js
+++ script/amongUs.js
@@ -3,21 +3,31 @@ const spots = [
     img: "assets/img/png/red-among.png",
     posX: "185px",
     posY: 0,
+    isImposter: false
   },
   {
     img: "assets/img/png/red-among.png",
     posX: 0,
     posY: 0,
+    isImposter: false
   },
   {
     img: "assets/img/png/red-among.png",
     posX: "185px",
     posY: "200px",
+    isImposter: false
   },
   {
     img: "assets/img/png/red-among.png",
     posX: "50px",
     posY: "200px",
+    isImposter: false
+  },
+  {
+    img: "assets/img/png/red-among.png",
+    posX: "300px",
+    posY: "300px",
+    isImposter: false
   },
 ];
 
@@ -25,7 +35,8 @@ const user = document.getElementById("user");
 const layer = document.getElementById("layer");
 const map = document.querySelector(".hiding-spots");
 
-var emergency = new Audio("/assets/sounds/emergencySound.mp3");
+let emergency = new Audio("/assets/sounds/emergencySound.mp3");
+let wrong = new Audio("/assets/sounds/wrong.mp3");
 
 user.innerHTML = localStorage.getItem("userName");
 
@@ -39,12 +50,40 @@ emergencyButton.addEventListener("click", () => {
   }, 1000);
 });
 
+// Création des spots et de la croix pour chaque spot
 map.innerHTML = spots
-  .map((spot) => {
-    return `<div id="spot" style="position: absolute; left:${spot.posX}; top:${spot.posY};">
-              <img src="${spot.img}" />
-            </div>`;
+  .map((spot, index) => {
+    return `
+      <div id="spot-${index}" class="spot" style="position: absolute; left:${spot.posX}; top:${spot.posY};">
+        <img src="${spot.img}" />
+        <img class="red-cross" src="/assets/img/png/cross-red.webp" />
+      </div>`;
   })
   .join("");
 
-const spot = document.getElementById("spot");
+// Fonction pour désigner un imposter aléatoirement
+const setImposter = () => {
+  let randomSpot = Math.floor(Math.random() * spots.length);
+  console.log("Spot imposter choisi:", randomSpot);
+  spots[randomSpot].isImposter = true;
+  console.log(spots);
+}
+
+setImposter();
+
+// Récupérer tous les spots
+const hiddingSpot = document.querySelectorAll(".spot");
+
+// Ajouter un événement de clic sur chaque spot
+hiddingSpot.forEach((spot, index) => {
+  spot.addEventListener("click", () => {
+    const cross = spot.querySelector(".red-cross");
+    if (spots[index].isImposter) {
+      console.log("C'est un imposter !");
+    } else {
+      console.log("Ce n'est pas un imposter.");
+      wrong.play()
+      cross.style.display = "block";
+    }
+  });
+});
